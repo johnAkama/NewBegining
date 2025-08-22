d1e6d2fadb73b59b549599d7bbc86dc4bb200dc4
Kodjo Akama <johnakama32@gmail.com>
2025-08-22 08:54:48 +0000

    testing conditional

--- learn.js
+++ learn.js
@@ -18,14 +18,12 @@ if(numberToCompared > 0) {
 
 a + b < 4 ? (result = 'Below') : (result = 'Over') */
 
-let message;
 
-if (login == 'Employee') {
-  message = 'Hello';
-} else if (login == 'Director') {
-  message = 'Greetings';
-} else if (login == '') {
-  message = 'No login';
-} else {
-  message = '';
-}
+let message =
+    login === 'Employee'
+        ? (message = 'Hello')
+        : login === 'Director'
+        ? (message = 'Greetings')
+        : (login = '' ? (message = 'No login') : (message = ''));
+
+alert(message);
