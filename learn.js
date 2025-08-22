730fb0c540a514441c2cedc4e481fcbd9f918b16
Kodjo Akama <johnakama32@gmail.com>
2025-08-22 08:18:45 +0000

    Learning javascript again

---
+++ learn.js
@@ -0,0 +1,23 @@
+'use strict';
+/* let programminName = prompt('Enter Javascript Official Name', 'Type here');
+programminName === 'ECMAScript'
+    ? alert('right')
+    : alert(`You don't know? “ECMAScript”!`) */
+
+/* let numberToCompared = prompt('Number', 'Enter number here');
+
+if(numberToCompared > 0) {
+    alert(1);
+} else if(numberToCompared < 0) {
+    alert(-1);
+} else {
+    alert(0);
+} */
+
+let result;
+
+if (a + b < 4) {
+    result = 'Below';
+} else {
+    result = 'Over';
+}
