87110fa25e3734b78a39a6893b03ac0c247af744
Kodjo Akama <johnakama32@gmail.com>
2025-12-16 16:01:03 +0000

    progress update

---
+++ mathBot.js
@@ -0,0 +1,5 @@
+/* let randomNum2 = Math.random() * (max - min) + min;
+console.log(randomNum2); */
+
+let num = 3.14159;
+console.log(num.toFixed());
---
+++ mathObject.js
@@ -0,0 +1,8 @@
+/* const randomNum = Math.random();
+
+console.log(randomNum); */
+
+const max = 20;
+const min = 1;
+const randomNum = Math.floor(Math.random() * (max - min + 1)) + min;
+console.log(randomNum);
