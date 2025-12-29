1bad80b30f5c5f0aa258ee5c53d6928c19516bb7
Kodjo Akama <johnakama32@gmail.com>
2025-12-29 04:29:13 +0000

    progress update

---
+++ letterDetection.js
@@ -0,0 +1,20 @@
+'use strict';
+
+function fearNotLetter(strg) {
+    let alphabet = 'abcdefghijklmnopqrstuvwxyz';
+    let newAlphabet = alphabet.split('');
+    for (let i = 0; i < alphabet.length; i++) {
+        if (
+            strg.indexOf(newAlphabet[i]) === -1 &&
+            strg.indexOf(newAlphabet[i - 1]) !== -1 &&
+            strg.indexOf(newAlphabet[i + 1]) !== -1
+        ) {
+            return newAlphabet[i];
+            break;
+        } else {
+            return undefined;
+        }
+    }
+}
+
+console.log(fearNotLetter('abcdefghijklmno'));
