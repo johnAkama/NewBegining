432cd42b1d4da551d3002c4fb8414f5333a8212d
Kodjo Akama <johnakama32@gmail.com>
2026-01-06 22:38:08 +0000

    progress update

---
+++ dndPair.js
@@ -0,0 +1,19 @@
+'use strict';
+
+function pairElement(word) {
+    let twoDA = [];
+
+    for (let i = 0; i < word.length; i++) {
+        if (word[i] === 'A') {
+            twoDA.push([word[i], 'T']);
+        } else if (word[i] === 'T') {
+            twoDA.push([word[i], 'A']);
+        } else if (word[i] === 'C') {
+            twoDA.push([word[i], 'G']);
+        } else if ([word[i] === 'G']) {
+            twoDA.push([word[i], 'C']);
+        }
+    }
+
+    return twoDA;
+}
