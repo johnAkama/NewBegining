715eabcd6d0b2f7f1c9e1c1a55d4b4bfb6474a14
Kodjo Akama <johnakama32@gmail.com>
2026-01-10 21:23:17 +0000

    Add files via upload

---
+++ chaining.js
@@ -0,0 +1,15 @@
+const transactions = [
+    { amount: 100, type: 'credit' },
+    { amount: 20, type: 'cash' },
+    { amount: 150, type: 'credit' },
+    { amount: 50, type: 'cash' },
+    { amount: 75, type: 'credit' },
+];
+
+const totalCredit = transactions.filter((item) => item.type === 'credit');
+let totalAmount = totalCredit.reduce(
+    (accumulator, currentAmount) => accumulator + currentAmount.amount*1.1,
+    0
+);
+
+console.log(totalAmount);
