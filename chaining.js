243dba107c75cc4ded601b341e57620103d9be57
Kodjo Akama <johnakama32@gmail.com>
2026-01-11 01:53:43 +0000

    progress update

--- chaining.js
+++ chaining.js
@@ -8,23 +8,7 @@ const transactions = [
 
 const totalCredit = transactions.filter((item) => item.type === 'credit');
 let totalAmount = totalCredit.reduce(
-    (accumulator, currentAmount) => accumulator + currentAmount.amount*1.1,
-    0
-);
-
-console.log(totalAmount);
-
-const transactions = [
-    { amount: 100, type: 'credit' },
-    { amount: 20, type: 'cash' },
-    { amount: 150, type: 'credit' },
-    { amount: 50, type: 'cash' },
-    { amount: 75, type: 'credit' },
-];
-
-const totalCredit = transactions.filter((item) => item.type === 'credit');
-let totalAmount = totalCredit.reduce(
-    (accumulator, currentAmount) => accumulator + currentAmount.amount*1.1,
+    (accumulator, currentAmount) => accumulator + currentAmount.amount * 1.1,
     0
 );
 
